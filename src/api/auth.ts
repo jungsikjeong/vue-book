ddf553adf0acc65eb68aac63f97b83fc17458dff
JungSik Jeong <wndtlr1024@gmail.com>
2024-01-27 16:10:47 +0900

    .

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -6,7 +6,15 @@ export const fetchKakaoAuth = async () => {
 
   const GRANT_TYPE = 'authorization_code';
   const CLIENT_ID = `${process.env.VUE_APP_KAKAO_REST_API}`;
-  const REDIRECT_URI = 'https://vue-book.vercel.app/callback/kakaotalk';
+  let REDIRECT_URI;
+
+  if (process.env.NODE_ENV === 'production') {
+    REDIRECT_URI = 'https://vue-book.vercel.app/callback/kakaotalk';
+  } else {
+    REDIRECT_URI = 'http://localhost:8080/callback/kakaotalk';
+  }
+
+  // const REDIRECT_URI = 'https://vue-book.vercel.app/callback/kakaotalk';
   // const  REDIRECT_URI ='http://localhost:8080/callback/kakaotalk'
 
   try {
--- src/components/social-login/KakaoLoginButton.vue
+++ src/components/social-login/KakaoLoginButton.vue
@@ -1,6 +1,12 @@
 <script setup lang="ts">
 const REST_API_KEY = process.env.VUE_APP_KAKAO_REST_API;
-const REDIRECT_URI = 'http://localhost:8080/callback/kakaotalk';
+let REDIRECT_URI;
+if (process.env.NODE_ENV === 'production') {
+  REDIRECT_URI = 'https://vue-book.vercel.app/callback/kakaotalk';
+} else {
+  REDIRECT_URI = 'http://localhost:8080/callback/kakaotalk';
+}
+// const REDIRECT_URI = 'http://localhost:8080/callback/kakaotalk';
 const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 </script>
 
